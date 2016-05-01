b20008f6cff93fd950e1fba4b5b7b8c2c0323f7b
Peter Trško <peter.trsko@gmail.com>
2016-05-01 22:21:31 +0200

    Example moved in to separate file

---
+++ Example.hs
@@ -0,0 +1,143 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+-- |
+-- Module:       $HEADER$
+-- Description:  TODO
+-- Copyright:    (c) 2016, Peter Trško
+-- License:      BSD3
+--
+-- Maintainer:   peter.trsko@gmail.com
+-- Stability:    experimental
+-- Portability:  DataKinds, FlexibleContexts, FlexibleInstances, GADTs,
+--               MultiParamTypeClasses, NoImplicitPrelude, PolyKinds,
+--               TemplateHaskell, TypeFamilies, TypeOperators,
+--               UndecidableInstances
+--
+-- TODO
+module Example
+  where
+
+import Prelude (Num((-)))
+
+import Control.Applicative (Applicative((<*>), pure))
+import Control.Concurrent
+    ( Chan
+    , MVar
+    , killThread
+    , newChan
+    , newEmptyMVar
+    , putMVar
+    , readChan
+    , readMVar
+    , threadDelay
+    , writeChan
+    )
+import Control.Monad (Monad((>>=)), (>>))
+import Data.Foldable (mapM_)
+import Data.Function (($), (.))
+import Data.Functor ((<$>))
+import Data.Int (Int)
+import Data.Maybe (maybe)
+import Data.Monoid ((<>))
+import Data.String (String)
+import System.IO (IO, putStrLn)
+import Text.Show (Show(show))
+
+import Data.Default.Class (Default(def))
+import Data.OverloadedLabels.TH (labels)
+import Data.OverloadedRecords ((:::), R, Rec(Rec), get)
+import Data.OverloadedRecords.TH (overloadedRecord)
+
+import Microservices
+
+
+data Logger = Logger {getLoggerChan :: Chan String}
+
+instance Show Logger where
+    show _ = "<<logger>>"
+
+data C = C
+    { _timeout :: Int
+    , _count :: Int
+    , _logger :: Logger
+    , _terminate :: MVar ()
+    }
+
+overloadedRecord def ''C
+
+labels ["snd", "timeout", "count", "logger", "terminate"]
+
+putLogMsg :: R '["logger" ::: Logger] r => r -> String -> IO ()
+putLogMsg = writeChan . getLoggerChan . get logger
+
+-- {{{ Client Service ---------------------------------------------------------
+
+type Client r = Svc r IO "Client" ClientRuntimeConfig
+
+type ClientRuntimeConfig =
+    '[ "timeout" ::: Int
+    , "count" ::: Int
+    , "logger" ::: Logger
+    , "terminate" ::: MVar ()
+    ]
+
+client :: Client r
+client = Svc $ \_svcName (Rec cfg) -> do
+    let putLogMsg' = putLogMsg cfg . ("Client: " <>)
+    putLogMsg' $ "timeout: " <> show (get timeout cfg)
+    putLogMsg' $ "count: " <> show (get count cfg)
+    putLogMsg' $ "logger: " <> show (get logger cfg)
+
+    loop cfg (get timeout cfg) (get count cfg)
+  where
+    loop cfg t = \case
+        0 -> putLogMsg cfg "Client: Done." >> putMVar (get terminate cfg) ()
+        n -> do
+            putLogMsg cfg ("Client: Ping: " <> show (get count cfg - n))
+            threadDelay t
+            loop cfg t (n - 1)
+
+-- }}} Client Service ---------------------------------------------------------
+
+-- {{{ Logging Service --------------------------------------------------------
+
+type Logging r = Svc r IO "Logging" '["logger" ::: Logger]
+
+loggingService :: Logging r
+loggingService = Svc $ \_svcName (Rec cfg) -> do
+    putLogMsg cfg $ "Logging: logger: " <> show (get logger cfg)
+    loop cfg
+  where
+    loop r = readLogMessage r >>= putStrLn >> loop r
+    readLogMessage = readChan . getLoggerChan . get logger
+
+-- }}} Logging Service --------------------------------------------------------
+
+main :: IO ()
+main = do
+    cfg <- mkCfg
+        <$> newLogger
+        <*> newEmptyMVar
+    threadIds <- runForkIO $ runServices cfg
+        loggingService
+        client
+    putLogMsg cfg $ "Main: " <> show threadIds
+    waitToTerminate cfg
+    killAll cfg threadIds
+  where
+    mkCfg = C 100000 10
+    newLogger = Logger <$> newChan
+    waitToTerminate cfg = readMVar $ get terminate cfg
+
+    killAll cfg tids = do
+        -- Wait for logger to finish writing messages.
+        threadDelay (get timeout cfg)
+        mapM_ (maybe (pure ()) killThread . get snd) tids
--- src/Microservices.hs
+++ src/Microservices.hs
@@ -35,16 +35,7 @@ import Data.Proxy (Proxy(Proxy))
 import GHC.TypeLits (KnownSymbol, SomeSymbol(SomeSymbol), Symbol)
 import System.IO (IO)
 
--- {{{ Test
-import Data.Bool (Bool(True))
-import System.IO (print)
-import Text.Show (Show)
-import Data.Default.Class (Default(def))
-import Data.OverloadedLabels.TH
-import Data.OverloadedRecords.TH (overloadedRecord)
--- }}} Test
-
-import Data.OverloadedRecords -- TODO: Explicit import list.
+import Data.OverloadedRecords (R, Rec(Rec))
 
 
 type ServiceMain r m name params = Proxy name -> Rec params r -> m ()
@@ -86,17 +77,17 @@ data Services r m (names :: [Symbol]) (params :: [[(Symbol, *)]]) where
         -> Services r m names params
         -> Services r m (name ': names) (ps ': params)
 
--- | Union of record constraints.
-type family UnionR (cs :: [[(Symbol, *)]]) (r :: *) where
-    UnionR '[]       r = R '[] r
-    UnionR (c ': cs) r = (R c r, UnionR cs r)
+-- | Union\/concatenation of record constraints.
+type family Rs (cs :: [[(Symbol, *)]]) (r :: *) where
+    Rs '[]       r = R '[] r
+    Rs (c ': cs) r = (R c r, Rs cs r)
 
 class RunServices e where
     type RuntimeConfiguration e :: *
     type ExecutionMonad e :: * -> *
 
     foldServices
-        :: UnionR params (RuntimeConfiguration e)
+        :: Rs params (RuntimeConfiguration e)
         => Services (RuntimeConfiguration e) (ExecutionMonad e) names params
         -> RuntimeConfiguration e
         -> e
@@ -144,52 +135,3 @@ instance RunServices (ForkIO r) where
             -> [(SomeSymbol, Maybe ThreadId)]
         mkResult svcName possiblyThreadId =
             ((SomeSymbol svcName, possiblyThreadId) :)
-
--- {{{ Test -------------------------------------------------------------------
-
-data BaseUrl = BaseUrl deriving Show
-data RestCredentials = RestCredentials deriving Show
-data Logger = Logger deriving Show
-
-type ApiClientRuntimeConfig =
-    '[ "restBaseUrl" ::: BaseUrl
-    , "restCredentials" ::: RestCredentials
-    , "logger" ::: Logger
-    ]
-
-type ApiClient r = Svc r IO "ApiClient" ApiClientRuntimeConfig
-
-data C = C
-    { _restBaseUrl :: BaseUrl
-    , _restCredentials :: RestCredentials
-    , _logger :: Logger
-    , _verbose :: Bool
-    }
-
-overloadedRecord def ''C
-
-labels ["restBaseUrl", "restCredentials", "logger", "verbose"]
-
-apiClient :: ApiClient r
-apiClient = Svc $ \_svcName (Rec cfg) -> do
-    print (get restBaseUrl cfg)
-    print (get restCredentials cfg)
-    print (get logger cfg)
-
-type Logging r = Svc r IO "Logging" '["logger" ::: Logger, "verbose" ::: Bool]
-
-loggingService :: Logging r
-loggingService = Svc $ \_svcName (Rec cfg) -> do
-    print (get logger cfg)
-    print (get verbose cfg)
-
-test :: IO ()
-test = do
-    r <- runForkIO $ runServices cfg
-        loggingService
-        apiClient
-    print r
-  where
-    cfg = C BaseUrl RestCredentials Logger True
-
--- }}} Test -------------------------------------------------------------------
